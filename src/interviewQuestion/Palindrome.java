a08963af5150f3b2f4c5a672aa8d61fc7a17f1b9
Sumit Jain <sumi12june@gmail.com>
2014-07-12 17:41:00 -0500

    Done

---
+++ src/interviewQuestion/Palindrome.java
@@ -0,0 +1,26 @@
+package interviewQuestion;
+
+public class Palindrome {
+
+	public Boolean isPalindrome(String strX){
+		if(strX.length()<2) return true;
+		
+		if(strX.charAt(0)==strX.charAt(strX.length()-1)){
+			isPalindrome(strX.substring(0, strX.length()-2));
+		}
+		else return false;
+		
+		return true;
+	}
+	public static void main(String args[]){
+		String S1 = "Sumit";
+		String S2 = "SumuS";
+		String S3 = "ABCDEFGHGFEDCBA";
+		String S4 = "Jain niaJ";
+		Palindrome p = new Palindrome();
+		System.out.println("Is "+ S1 + " Palindrome ??? :" + p.isPalindrome(S1));
+		System.out.println("Is "+ S2 + " Palindrome ??? :" + p.isPalindrome(S2));
+		System.out.println("Is "+ S3 + " Palindrome ??? :" + p.isPalindrome(S3));
+		System.out.println("Is "+ S4 + " Palindrome ??? :" + p.isPalindrome(S4));
+	}
+}

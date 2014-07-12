5b5d33d274ce45a990eb425efe41bc970b412c1c
Sumit Jain <sumi12june@gmail.com>
2014-07-12 15:06:17 -0500

    Done

--- src/interviewQuestion/FindElementInSorted2DArray.java
+++ src/interviewQuestion/FindElementInSorted2DArray.java
@@ -2,10 +2,8 @@ package interviewQuestion;
 //Here objective is find an element in two dimentional array
 //all rows and columns of an array are sorted in ascending order respectively
 public class FindElementInSorted2DArray {
-	int [][] arrA;
-	int number;
-	
-	public Boolean findElement(){
+		
+	public Boolean findElement(int [][] arrA, int number){
 		//start from the left top corner, say ele;
 		//if ele>number -> move left
 		//if ele<number -> move right
@@ -13,15 +11,30 @@ public class FindElementInSorted2DArray {
 		int row = 0;
 		int col = arrA[1].length-1;
 		boolean numberFound = false;
+		System.out.print("The Movement : " );
 		while(numberFound==false){
 			int ele = arrA[row][col];
-			if(ele==number)return true;
+			System.out.print(ele + "->" );
+			if(ele==number){				
+				return true;
+			}
 			else if(ele>number)col--;
-			else if (ele>number)row++;
+			else if (ele<number)row++;
 			if(row>arrA[0].length-1||col<0)return false;
 		}
-		return false;
-		
+		return false;		
 	}
-
+	
+	public static void main(String args[]){
+		int [][] a = {{1,2,3,4},{6,7,8,9},{13,14,15,16},{17,18,19,20},{22,23,24,26}};
+		int number1 = 15;
+		int number2 = 5;
+		int number3 = 19;
+		int number4 = 25;
+		FindElementInSorted2DArray f = new FindElementInSorted2DArray();
+		System.out.println("The "+ number1 + " present in 2D array a ??? :" + f.findElement(a, number1));
+		System.out.println("The "+ number2 + " present in 2D array a ??? :" + f.findElement(a, number2));
+		System.out.println("The "+ number3 + " present in 2D array a ??? :" + f.findElement(a, number3));
+		System.out.println("The "+ number4 + " present in 2D array a ??? :" + f.findElement(a, number4));
+	}
 }

87ec6ea1aacfe75d9a176d3eca5e111ac28b8986
Sumit Jain <sumi12june@gmail.com>
2014-07-16 21:18:52 -0500

    Find the longest sequence of prefix
    Find the longest sequence of prefix shared by all the words in a string.
    "abcdef abcdxxx abcdabcdef abcyy" => "abc"

---
+++ src/interviewQuestion/LongestPrefixSequence.java
@@ -0,0 +1,27 @@
+package interviewQuestion;
+
+public class LongestPrefixSequence {
+	private String [] arrA;
+	public LongestPrefixSequence(String [] arrA){
+		this.arrA = arrA;
+	}
+	public String findPrefix(){
+		int resultLen = arrA[0].length();
+		int curr;
+		for(int i=1;i<arrA.length;i++){
+			curr=0;
+			while(curr<resultLen && curr<arrA[i].length() && arrA[0].charAt(curr)==arrA[1].charAt(curr)){
+				curr++;
+			}
+			resultLen = curr;
+		}
+		return arrA[0].substring(0,resultLen);
+	}
+	public static void main(String args[]){
+		String [] arrA = {"Sumit","Summation","Summit","Sum"};
+		LongestPrefixSequence lp = new LongestPrefixSequence(arrA);
+		System.out.println("Common Prefix is : " + lp.findPrefix());
+		
+	}
+
+}

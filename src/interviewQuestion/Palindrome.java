0516244920438e811f7d2d84711a31cb67b009b3
Sumit Jain <sumi12june@gmail.com>
2014-07-22 17:45:07 -0700

    Fix

--- src/interviewQuestion/Palindrome.java
+++ src/interviewQuestion/Palindrome.java
@@ -8,21 +8,23 @@ public class Palindrome {
 		if(strX.length()<2) return true;
 		
 		if(strX.charAt(0)==strX.charAt(strX.length()-1)){
-			isPalindrome(strX.substring(0, strX.length()-2));
+			return isPalindrome(strX.substring(1, strX.length()-1));
 		}
 		else return false;
 		
-		return true;
+		
 	}
 	public static void main(String args[]){
 		String S1 = "Sumit";
 		String S2 = "SumuS";
 		String S3 = "ABCDEFGHGFEDCBA";
 		String S4 = "Jain niaJ";
+		String S5 = "JasasddfwfcsJ";
 		Palindrome p = new Palindrome();
 		System.out.println("Is "+ S1 + " Palindrome ??? :" + p.isPalindrome(S1));
 		System.out.println("Is "+ S2 + " Palindrome ??? :" + p.isPalindrome(S2));
 		System.out.println("Is "+ S3 + " Palindrome ??? :" + p.isPalindrome(S3));
 		System.out.println("Is "+ S4 + " Palindrome ??? :" + p.isPalindrome(S4));
+		System.out.println("Is "+ S5 + " Palindrome ??? :" + p.isPalindrome(S5));
 	}
 }
